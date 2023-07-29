464057b761d1c0184b924c6c896f3ac68372faaa
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2023-07-29 15:25:50 +0300

    arrays

---
+++ DataAndAlgorithm/src/ArrayUtil.java
@@ -0,0 +1,31 @@
+public class ArrayUtil {
+    public void printArray(int arr[]) {
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+    }
+
+
+    /*
+    public void arrayDemo() {
+        int[] myArray = new int[5];
+        myArray[0] = 3;
+        myArray[1] = 7;
+        myArray[2] = 11;
+        myArray[3] = 2;
+        myArray[4] = 10;
+
+        printArray(myArray);
+        System.out.println(myArray.length);
+
+        int[] arr = {1,2,3,4};
+        printArray(arr);
+    }
+     */
+
+
+
+}
--- DataAndAlgorithm/src/Main.java
+++ DataAndAlgorithm/src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        ArrayUtil arrUtil = new ArrayUtil();
+        // arrUtil.arrayDemo();
+        arrUtil.printArray(new int[] {2,11,32,4,6});
+    }
 }
