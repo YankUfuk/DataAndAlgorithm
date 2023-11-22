1eeed871420c5cec363fa845e6af4467bf4e06fc
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2023-11-22 12:55:15 +0300

    some exercises

--- DataAndAlgorithm/src/DataAndAlgorithmExercises/src/Bubblesort.java
+++ DataAndAlgorithm/src/DataAndAlgorithmExercises/src/Bubblesort.java
@@ -1,2 +1,39 @@
+import java.util.Random;
+
 public class Bubblesort {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100);
+        }
+
+        System.out.println("Before swap: ");
+        print(numbers);
+
+        boolean isSwapped = true;
+
+        while (isSwapped) {
+            isSwapped = false;
+            for (int i = 0; i < numbers.length-1; i++) {
+                if (numbers[i] > numbers[i+1]) {
+                    isSwapped = true;
+                    int temp = numbers[i];
+                    numbers[i] = numbers[i+1];
+                    numbers[i+1] = temp;
+
+                }
+            }
+        }
+
+        System.out.println("After swap: ");
+        print(numbers);
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
--- DataAndAlgorithm/src/DataAndAlgorithmExercises/src/SelectionSort.java
+++ DataAndAlgorithm/src/DataAndAlgorithmExercises/src/SelectionSort.java
@@ -1,2 +1,42 @@
+import java.util.Random;
+
 public class SelectionSort {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100);
+        }
+
+        System.out.println("Before sorting");
+        print(numbers);
+        selectionSort(numbers);
+        System.out.println("After Sorting");
+        print(numbers);
+    }
+    static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = array[i];
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minIndex = j;
+                }
+            }
+            swap(array, i, minIndex);
+        }
+    }
+    static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
